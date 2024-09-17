d4d709af994103c66e5f55432c5be05133db4b5c
arofik21 <arofikmohammad21@gmail.com>
2024-09-17 14:26:36 +0800

    task

---
+++ src/main/java/com/arofik/Day10/Book.java
@@ -0,0 +1,15 @@
+package com.arofik.Day1;
+
+public class Book extends LibraryMaterial {
+    private String author;
+
+    public Book(String title, String author, int totalCopies) {
+        super(title, totalCopies);
+        this.author = author;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Book: " + title + " by " + author + " | Available copies: " + availableCopies);
+    }
+}
---
+++ src/main/java/com/arofik/Day10/DVD.java
@@ -0,0 +1,15 @@
+package com.arofik.Day10;
+
+public class DVD extends LibraryMaterial {
+    private int duration;  // duration in minutes
+
+    public DVD(String title, int duration, int totalCopies) {
+        super(title, totalCopies);
+        this.duration = duration;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("DVD: " + title + " | Duration: " + duration + " mins | Available copies: " + availableCopies);
+    }
+}
